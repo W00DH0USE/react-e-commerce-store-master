c670f54c0875d9a84ec05f36bef2ba9abc9a602c
Iyron Woodhouse <iyronwoodhouse@gmail.com>
2020-02-05 12:31:26 -0800

    Finished Product

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,10 +1,35 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {Link} from "react-router-dom";
+import {ProductConsumer} from "../context";
 
 export default class Product extends Component {
   render() {
+    const {id, title, img, price, inCart} = this.props.product;
     return (
-      <div>
-        <h3>Hello from Product</h3>
+      <div className="productWrapper">
+        <div className="card">
+          <div className="image-container" onClick={() => console.log("You clicked the image")}>
+            <Link to="/details">
+              <img src={img} alt="product" className="card-image" />
+            </Link>
+            <button className="cart-btn" disabled={inCart ? true : false} onClick={() => {console.log("Added to the cart")}}>
+              {inCart ? 
+                (<p className="text-capitalize mb-0" disabled>{" "}in cart</p>) 
+                : 
+                (<i className="fas fa-cart-plus"></i>)
+              }
+            </button>
+          </div>
+          <div className="card-info">
+            <p className="card-title">
+              {title}
+            </p>
+            <h5 className="text-blue card-price">
+              <span>$</span>
+              {price}
+            </h5>
+          </div>
+        </div>
       </div>
     )
   }
