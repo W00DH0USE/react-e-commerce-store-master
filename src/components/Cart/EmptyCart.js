f40235ea8a04059f4e68cdd1c2ccdeffbb7a1e80
Iyron Woodhouse <iyronwoodhouse@gmail.com>
2020-02-05 18:25:02 -0800

    Finished EmptyCart

---
+++ src/components/Cart/EmptyCart.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function EmptyCart() {
+  return (
+    <div className="emptycart-container container">
+      <div className="row">
+        <div className="emptycart-title text-title">
+          <h1>your cart is empty</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
