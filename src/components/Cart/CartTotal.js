855d7c14b7435332fa68071d4b4854ea9a5b7d18
Iyron Woodhouse <iyronwoodhouse@gmail.com>
2020-02-06 16:06:04 -0800

    Fixed css errors

--- src/components/Cart/CartTotal.js
+++ src/components/Cart/CartTotal.js
@@ -8,9 +8,9 @@ export default function CartTotal({value}) {
     <React.Fragment>
       <div className="container">
         <div className="row">
-          <div className="col-10 mt-2 ml-sm-5 ml-md-auto col-sm-8 text-capitalize text-right">
+          <div className="cartTotal-container">
             <Link to="/">
-              <button className="btn btn-outline-danger text-uppercase mb-3 px-5" type="button" onClick={() => clearCart()}>
+              <button className="buttonContainer buttonContainer-red" type="button" onClick={() => clearCart()}>
                 clear cart
               </button>
             </Link>
--- src/components/Default.js
+++ src/components/Default.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 
 export default class Default extends Component {
   render() {
+    console.log()
     return (
-      <div>
-        <h3>Page Not Found</h3>
+      <div className="container">
+        <div className="row">
+          <div className="default-container">
+            <div className="text-404">
+              <h1 style={{color: "red"}}>404</h1>
+              <h1>Page Not Found!</h1>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
